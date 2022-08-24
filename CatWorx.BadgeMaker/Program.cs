67c3b28c8ea6fdac0589978ff26f4d67af6eebac
imjord <dean.jay.goat@gmail.com>
2022-08-23 21:50:48 -0600

    have employee class and creating util class to store the employee data

---
+++ CatWorx.BadgeMaker/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+  class Program
+  {
+   
+    // LIST EMPLOYEES METHOD
+    static List<Employee> GetEmployees(){
+        // get list of employees
+        List<Employee> employees = new List<Employee>();
+        while(true)
+        {
+            Console.WriteLine("Please enter first name of employee: (leave empty to exit): ");
+            string firstName = Console.ReadLine() ?? "";
+            if(firstName == ""){
+                break;
+            }
+            Console.Write("Enter a last name: ");
+            string lastName = Console.ReadLine() ?? "";
+            Console.Write("Enter employee id: ");
+            int id = Int32.Parse(Console.ReadLine() ?? "");
+            Console.Write("Enter employee photo url: ");
+            string photoUrl = Console.ReadLine() ?? "";
+
+            // employee instance
+            Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+            employees.Add(currentEmployee);
+        }
+        return employees;
+    }
+
+     
+
+        // MAIN
+     static void Main(string[] args)
+    {
+        // employee class 
+        List<Employee> employees = GetEmployees();
+
+        Util.PrintEmployees(employees);
+    }
+  }
+}
