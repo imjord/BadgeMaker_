1a35f1763cb131ec0038802fd3fb5c6b63207dcb
imjord <dean.jay.goat@gmail.com>
2022-08-25 13:24:24 -0600

    added exit

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -11,13 +11,15 @@ namespace CatWorx.BadgeMaker
      async static Task Main(string[] args)
     {
         // employee class 
-        Console.Write("Would you like to randomly generate [1] or manually generate [2] ? (please press 1 or 2)");
+        Console.Write("Would you like to randomly generate [1] or manually generate [2] ? (please press 1 or 2, leave empty to exit)");
         string userInput = Console.ReadLine() ?? "";
         if(userInput == "1"){
           List<Employee> employees = await PeopleFetcher.GetFromApi();
           Util.PrintEmployees(employees);
         Util.MakeCSV(employees);
         await Util.MakeBadges(employees);
+        } if(userInput == ""){
+          return;
         } else {
           List<Employee> employees =  PeopleFetcher.GetEmployees();
           Util.PrintEmployees(employees);
