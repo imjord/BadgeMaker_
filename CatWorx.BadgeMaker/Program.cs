f2f0453ccc51eb15cb0c7fb51faf034114b8da07
imjord <dean.jay.goat@gmail.com>
2022-08-25 13:03:00 -0600

    able to automatically create badges from api

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -6,39 +6,12 @@ namespace CatWorx.BadgeMaker
 {
   class Program
   {
-   
-    // LIST EMPLOYEES METHOD
-    static List<Employee> GetEmployees(){
-        // get list of employees
-        List<Employee> employees = new List<Employee>();
-        while(true)
-        {
-            Console.WriteLine("Please enter first name of employee: (leave empty to exit): ");
-            string firstName = Console.ReadLine() ?? "";
-            if(firstName == ""){
-                break;
-            }
-            Console.Write("Enter a last name: ");
-            string lastName = Console.ReadLine() ?? "";
-            Console.Write("Enter employee id: ");
-            int id = Int32.Parse(Console.ReadLine() ?? "");
-            Console.Write("Enter employee photo url: ");
-            string photoUrl = Console.ReadLine() ?? "";
-
-            // employee instance
-            Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-            employees.Add(currentEmployee);
-        }
-        return employees;
-    }
-
-     
-
-        // MAIN
+    
+      // MAIN
      async static Task Main(string[] args)
     {
         // employee class 
-        List<Employee> employees = GetEmployees();
+        List<Employee> employees = await PeopleFetcher.GetFromApi();
 
         Util.PrintEmployees(employees);
         Util.MakeCSV(employees);
